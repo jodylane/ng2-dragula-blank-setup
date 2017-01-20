5930763dbf8b54b61e4c5e1be4d76ae9f2e2d8d9
Josh Lane <jodylane@umail.iu.edu>
2017-01-20 17:29:47 -0500

    dragula basic setup for puzzle

---
+++ src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import { DragulaService } from '../../../node_modules/ng2-dragula/ng2-dragula';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  providers: [DragulaService]
+})
+export class HomePage {
+
+  bucket1 = [];
+  bucket2 = [];
+
+  constructor(public navCtrl: NavController, private dragulaService: DragulaService, private alertCtrl: AlertController) {
+    for(var i = 0; i < 20; i++){
+      this.bucket1.push('1.<' + i + '>');
+      this.bucket2.push('2.<' + i + '>');
+    }
+    dragulaService.drop.subscribe((value)=>{
+      let prompt = this.alertCtrl.create({
+        title: 'item mpved',
+        subTitle: 'so much fun!',
+        buttons:['OK']
+      });
+      prompt.present();
+    });
+  }
+
+}
