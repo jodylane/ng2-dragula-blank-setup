fada9a5ab5de3400ca9864892e66e531a21d9777
Josh Lane <jodylane@umail.iu.edu>
2017-01-21 22:53:45 -0500

    Debugging this will break

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,28 +2,63 @@ import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
 import { DragulaService } from '../../../node_modules/ng2-dragula/ng2-dragula';
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
-  providers: [DragulaService]
+    selector: 'page-home',
+    templateUrl: 'home.html',
+    providers: [DragulaService]
 })
 export class HomePage {
+    food = [{
+        "name": "Pineapple",
+        "picture": "http://www.foodlecompany.com/wp-content/uploads/2015/04/pineapple.jpg"
+    }, {
+        "name": "Mango",
+        "picture": "M"
+    }, {
+        "name": "Grapes",
+        "picture": "G"
+    }, {
+        "name": "Strawberries",
+        "picture": "S"
+    },];
+    animals = [{
+        "name": "Cow",
+        "picture": "C"
+    }, {
+        "name": "Chicken",
+        "picture": "C"
+    }, {
+        "name": "Dog",
+        "picture": "D"
+    }, {
+        "name": "Cat",
+        "picture": "C"
+    },];
+    bucket1 = [];
+    bucket2 = [];
 
-  bucket1 = [];
-  bucket2 = [];
+    constructor(public navCtrl:NavController, private dragulaService:DragulaService, private alertCtrl:AlertController) {
 
-  constructor(public navCtrl: NavController, private dragulaService: DragulaService, private alertCtrl: AlertController) {
-    for(var i = 0; i < 20; i++){
-      this.bucket1.push('1.<' + i + '>');
-      this.bucket2.push('2.<' + i + '>');
+        for (var i = 0; i < this.food.length; i++) {
+            console.log(i % 2);
+            if (i % 2 == 0) {
+                this.bucket1.push(this.food[i]);
+                this.bucket1.push(this.animals[i]);
+            } else if (i % 2 == 1) {
+                this.bucket2.push(this.food[i]);
+                this.bucket2.push(this.animals[i]);
+            }
+
+
+        }
+        dragulaService.drop.subscribe((value)=> {
+            this.onDrop(value.slice(1));
+            let prompt = this.alertCtrl.create({
+                title: 'item moved',
+                subTitle: 'so much fun!',
+                buttons: ['OK']
+            });
+            prompt.present();
+        });
     }
-    dragulaService.drop.subscribe((value)=>{
-      let prompt = this.alertCtrl.create({
-        title: 'item moved',
-        subTitle: 'so much fun!',
-        buttons:['OK']
-      });
-      prompt.present();
-    });
-  }
 
 }
