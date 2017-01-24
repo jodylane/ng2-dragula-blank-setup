89c9e929fc76f1a8f28262bd1fc24134ed6408b2
jodylane <jodylane@umail.iu.edu>
2017-01-23 21:42:52 -0500

    adding memory game status: broken

--- src/pages/memory/memory.ts
+++ src/pages/memory/memory.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams, AlertController} from 'ionic-angular';
 
 /*
  Generated class for the Memory page.
@@ -14,42 +14,67 @@ import { NavController, NavParams, AlertController } from 'ionic-angular';
 })
 export class MemoryPage {
     colors = [];
-    memorySquares= [];
-    constructor(public navCtrl:NavController, public navParams:NavParams, public alertCtrl:AlertController) {
+    memorySquares = [];
+    match = [];
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
         this.startGame();
     }
-    startGame():void{
+
+    startGame(): void {
         this.colors = [
             {
+                "id": "",
                 "colorName": "blue"
             },
             {
+                "id": "",
                 "colorName": "red"
             },
             {
+                "id": "",
                 "colorName": "green"
             },
             {
+                "id": "",
                 "colorName": "yellow"
             }
         ];
-            for(let i = 0;i < 4;i++){
-                let rand = Math.floor(Math.random() * this.colors.length);
-                let color = this.colors[rand].colorName;
-                this.memorySquares.push(color);
-                this.memorySquares.push(color);
-                this.colors.splice(rand,1);
-                console.log(rand,color)
-                console.log(this.colors[rand],this.colors)
-            }
-            console.log(this.memorySquares);
-        this.memorySquares.sort(()=>{
+        for (let i = 0; i < 4; i++) {
+            let rand = Math.floor(Math.random() * this.colors.length);
+            let color = this.colors[rand];
+            this.memorySquares.push(color);
+            this.memorySquares.push(color);
+            this.colors.splice(rand, 1);
+        }
+
+        for(let i = 0;i < this.memorySquares.length;i++){
+             this.memorySquares[i].id = this.memorySquares[i].colorName + i;
+
+
+            console.log(this.memorySquares[i].id,this.memorySquares.length)
+        }
+
+        //random color assortment
+        console.log("randbefore: ",this.memorySquares);
+        this.memorySquares.sort(() => {
             return 0.5 - Math.random();
-        })
+        });
+        //randomize random assortment
         console.log(this.memorySquares)
     }
-    restartGame():void{
+
+    restartGame(): void {
         this.memorySquares = [];
         this.startGame();
+    }
+    matchMaker(item):void{
+        this.match.push(item);
+        if(this.match[0] == this.match[1]){
+
+        }else if(this.match[0] == this.match[1]){
+
+        }
+        console.log(this.match)
     }
 }
